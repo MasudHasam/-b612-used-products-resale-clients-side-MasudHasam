ce99738d9dd0b5670430133c2b5f9f5d706656fb
Masud Hasan <masudhasamf@gmail.com>
2022-11-27 22:25:23 +0600

    some important dashboard work done

--- src/Layout/DashboardLayout/DashboardLayout.jsx
+++ src/Layout/DashboardLayout/DashboardLayout.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Footer } from 'react-daisyui';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../../Pages/Shared/Navbar/Navbar';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../Context/AuthProvider';
 
 const DashboardLayout = () => {
+    const { user, loginUser } = useContext(AuthContext);
+    console.log(loginUser);
     return (
         <div>
             <Navbar></Navbar>
@@ -16,11 +19,24 @@ const DashboardLayout = () => {
                 <div className="drawer-side">
                     <label htmlFor="dashboardMenue" className="drawer-overlay"></label>
                     <ul className="menu p-4 w-80 text-base-content">
-                        <li><Link className='btn btn-outline mb-1 hover:mx-[1.5px] rounded-md' to='/dashboard'>My Orders</Link></li>
-                        <li><Link className='btn btn-outline mb-1 hover:ml-[1.5px] rounded-md' to='/dashboard/addproduct'>Add Product</Link></li>
-                        <li><Link className='btn btn-outline mb-1 hover:ml-[1.5px] rounded-md' to='/dashboard/myproducts'>My Product</Link></li>
-                        <li><Link className='btn btn-outline mb-1 hover:ml-[1.5px] rounded-md' to='/dashboard/allusers'>All Users</Link></li>
-                        <li><Link className='btn btn-outline mb-1 hover:ml-[1.5px] rounded-md' to='/dashboard/allbuyers'>All Buyers</Link></li>
+                        {
+                            loginUser?.options === 'Buyer' &&
+                            <li><Link className='btn btn-outline mb-1 hover:mx-[1.5px] rounded-md' to='/dashboard'>My Orders</Link></li>
+                        }
+                        {
+                            loginUser?.options === 'Seller' &&
+                            <>
+                                <li><Link className='btn btn-outline mb-1 hover:ml-[1.5px] rounded-md' to='/dashboard/addproduct'>Add Product</Link></li>
+                                <li><Link className='btn btn-outline mb-1 hover:ml-[1.5px] rounded-md' to='/dashboard/myproducts'>My Product</Link></li>
+                            </>
+                        }
+                        {
+                            loginUser?.role === 'Admin' &&
+                            <>
+                                <li><Link className='btn btn-outline mb-1 hover:ml-[1.5px] rounded-md' to='/dashboard/allusers'>All Users</Link></li>
+                                <li><Link className='btn btn-outline mb-1 hover:ml-[1.5px] rounded-md' to='/dashboard/allbuyers'>All Buyers</Link></li>
+                            </>
+                        }
                     </ul>
 
                 </div>
